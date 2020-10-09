91669f7a886e355dfa78ac5a9019873c18470933
Jorge <j.perezu@alumnos.upm.es>
2020-10-09 14:18:55 +0200

    upload for heroku v3

--- local/settings.py
+++ local/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '34_bi#x1*bv&kl(#@scz35e0eo#$)(zm(v4an--=6uw!lh#1go'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
