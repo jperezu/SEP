13981e9981f1d336fb68eef883ca2e94eddaa5d1
Jorge <j.perezu@alumnos.upm.es>
2020-10-09 14:23:22 +0200

    upload for heroku v3

--- local/wsgi.py
+++ local/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 #  from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "local.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "local.settings")
 
 application = get_wsgi_application()
 #  application = DjangoWhiteNoise(application)
