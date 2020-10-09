5ff3d86f29ba09a90ba21fd1a5f838058b50d441
Jorge <j.perezu@alumnos.upm.es>
2020-10-09 16:31:20 +0200

    admin access granted to new models

--- SEP/admin.py
+++ SEP/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Client, Event, RequestEvent
+from .models import Client, Event, RequestEvent, RequestRecruitment, RequestBudget, Task
 
 admin.site.register(Client)
 admin.site.register(Event)
 
 admin.site.register(RequestEvent)
+admin.site.register(RequestRecruitment)
+admin.site.register(RequestBudget)
+
+admin.site.register(Task)
