df754b7ca4dac7f1bcdf406416527a60ac670c4f
Jorge <j.perezu@alumnos.upm.es>
2020-10-09 13:47:03 +0200

    upload for heroku v3

--- local/settings.py
+++ local/settings.py
@@ -123,7 +123,7 @@ LOGOUT_REDIRECT_URL = '/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
