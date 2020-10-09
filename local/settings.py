ca1af53c5bb70151cda29bc9e89404754f481a24
Jorge <j.perezu@alumnos.upm.es>
2020-10-09 14:55:23 +0200

    upload for heroku v4

--- local/settings.py
+++ local/settings.py
@@ -39,7 +39,7 @@ DATABASES = {
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES.update(default=db_from_env)
 
 # Application definition
 
